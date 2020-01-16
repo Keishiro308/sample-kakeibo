db141fd2d8ab140e3a33a1cf767cb341db484a82
Keishiro Nishiyama <kn11308@gmail.com>
2020-01-17 02:56:00 +0900

    improve design

--- app/assets/javascripts/item_form.js
+++ app/assets/javascripts/item_form.js
@@ -1,7 +1,9 @@
 $(function(){
   let name_field = $('#item_name');
   name_field.on('focus', function(){
-    name_field.next('label').show();
+    let error_msg = $(this).next('span.error_msg');
+    error_msg.fadeOut();
+    $(this).next('label').show();
   });
   name_field.on('blur' ,function(){
     let val = name_field.val();
@@ -19,12 +21,20 @@ $(function(){
     }
   });
   value_field.on('focus', function(){
-    value_field.next('label').show();
+    let error_msg = $('#value_field_wrap').nextAll('.error_message').find('span');
+    error_msg.remove();
+    $(this).next('label').show();
   });
   value_field.on('blur' ,function(){
     let val = value_field.val();
     if (val === '' || val.match(/\s+/) || val.match(/\D+/)) {
+      let error_msg_wrap = $('#value_field_wrap').nextAll('.error_message')
       value_field.addClass('is_error')
+      if (val.match(/\D+/)) {
+        error_msg_wrap.html('<span class="error_msg">入力できるのは半角数字のみです</span>');
+      }else{
+        error_msg_wrap.html('<span class="error_msg">入力してください</span>');
+      }
     }else {
       value_field.removeClass('is_error')
       value_field.next('label').fadeOut();
@@ -32,22 +42,34 @@ $(function(){
   });
 
   let select = $('#select');
-  select.on('blur' ,function(){
+  let select_message_wrap = $('label.select').nextAll('.error_message');
+  select.on('change' ,function(){
     let val = select.val();
-    if (val === '' || val === undefined) {
-      select.addClass('is_error')
+    let error_msg = select_message_wrap.find('span');
+    if (val === '') {
+      select.addClass('is_error');
+      select_message_wrap.html('<span class="error_msg">入力してください</span>');
     }else {
-      select.removeClass('is_error')
+      select.removeClass('is_error');
+      error_msg.remove();
     }
   });
 
   let item_date = $('#item_date');
+  let date_error_wrap = $('.date_field').find('.error_message')
   item_date.on('blur' ,function(){
     let val = item_date.val();
     if (val === '' || val.match(/\s+/)) {
-      item_date.addClass('is_error')
-    }else {
-      item_date.removeClass('is_error')
+      item_date.addClass('is_error');
+      date_error_wrap.html('<span class="error_msg">入力してください</span>');
+    }
+  });
+  item_date.on('change', function(){
+    let val = item_date.val();
+    if (val === '' || val.match(/\s+/)) {
+    }else{
+      item_date.removeClass('is_error');
+      date_error_wrap.find('span').remove();
     }
   });
 });
