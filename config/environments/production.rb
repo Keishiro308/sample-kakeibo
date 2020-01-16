f498cda03c0ed9bc569d797bb12568a4dbe15681
Keishiro Nishiyama <kn11308@gmail.com>
2020-01-16 21:23:29 +0900

    fix mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,10 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "kakeibo-prototype_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings ={ :domain => 'kakeibo-kei.herokuapp.com/' }
   config.action_mailer.default_url_options = { host: 'https://kakeibo-kei.herokuapp.com/' }
+  config.log_level = :warn
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
